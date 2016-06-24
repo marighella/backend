d6076dafe68e57d8679b64a30cdda15ce791d184
Rodrigo Alencar <rodrigovieira18@gmail.com>
2016-06-24 12:40:03 -0300

    Adding basic end points

--- spec/api/news.spec.js
+++ spec/api/news.spec.js
@@ -12,10 +12,31 @@ describe('News API', ()=>{
     app = server.start();
   });
 
+  afterEach(()=>{
+    app.close();
+  });
+
   it('should be alive', (done)=>{
     request(app)
       .get('/news')
       .expect(200)
+      .end(fails(done));
+  });
+
+  it('should save a document', (done)=>{
+    var json = JSON.stringify({filename: 'arquivo.md', metadata: { created_date: '2014-01-30'}});
+    request(app)
+      .post('/news')
+      .send(json)
+      .expect(201)
+      .end(fails(done));
+  });
+
+  it('should return a document', (done)=>{
+    var json = JSON.stringify({filename: 'arquivo.md', metadata: { created_date: '2014-01-30'}});
+    request(app)
+      .get('/news/1')
+      .expect(200,json)
       .end(fails(done));
   });
 });
