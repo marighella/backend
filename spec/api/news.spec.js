abd5127b5f337d53808e0b2c853dc4a3ed8d8d9f
Rodrigo Alencar <rodrigovieira18@gmail.com>
2016-08-21 23:26:15 -0300

    Add endpoint to get a list of all news

--- spec/api/news.spec.js
+++ spec/api/news.spec.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var server = require('../../lib/server'),
-    news   = require('../../lib/news'),
-    fails  = require('../helpers').fails,
-    db     = require('../../lib/db').start('jasmine'),
-   request = require('supertest');
+var server  = require('../../lib/server'),
+    news    = require('../../lib/news'),
+    fails   = require('../helpers').fails,
+    db      = require('../../lib/db').start('jasmine'),
+    Promise = require('bluebird'),
+    assert  = require("assert"),
+    request = require('supertest');
 
 
 describe('News API', ()=>{
@@ -13,6 +15,7 @@ describe('News API', ()=>{
   beforeEach(()=>{
     app = server.start();
     db.connection.db.dropDatabase();
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
   });
 
   afterEach(()=>{
@@ -37,15 +40,51 @@ describe('News API', ()=>{
 
   it('should return a document', (done)=>{
     var data = {filename: 'arquivo.md', metadata: { created_date: '2014-01-30'}};
-    var obj = news.save(data)[1];
+    var obj  = undefined;
 
-    request(app)
-      .get('/news/'+obj.id)
-      .expect(function(res) {
-         if (!('filename' in res.body)) throw new Error("missing filename key");
-         if (!('metadata' in res.body)) throw new Error("missing metadata key");
+    news.save(data).then(function(obj){
+      request(app)
+        .get('/news/'+obj.id)
+        .expect(function(res) {
+          if (!('filename' in res.body)) throw new Error("missing filename key");
+          if (!('metadata' in res.body)) throw new Error("missing metadata key");
+        })
+        .end(fails(done));
+    });
+  });
 
-      })
-      .end(fails(done));
+  it('should return a list of documents', (done)=>{
+    var data = [
+      {
+       filename: 'arquivo-old.md',
+       metadata: {
+         created_date: '2014-01-30'
+       }
+      },
+      {
+       filename: 'arquivo-new.md',
+       metadata: {
+         created_date: '2015-01-30'
+       }
+      }
+    ];
+
+    var promises = [];
+    data.forEach(function(element, index){
+      promises.push(news.save(element));
+    });
+
+
+    Promise.all(promises).then(function(){
+      request(app)
+        .get('/news')
+        .expect(200)
+        .end(function(err, res){
+          var result = res.body;
+          assert.equal(result[0].filename, 'arquivo-new.md');
+          assert.equal(result[1].filename, 'arquivo-old.md');
+          done();
+        });
+    });
   });
 });
